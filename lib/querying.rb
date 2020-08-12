fcdfeb439f019952815ce9c083d4674540e649d8
Mattie43 <ericksenm93@ymail.com>
2020-08-12 14:39:47 -0400

    done

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,46 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "SELECT books.title, books.year FROM books
+   JOIN series
+   ON books.series_id = series.id
+   WHERE books.series_id = 1
+   ORDER BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT name, motto FROM characters
+   ORDER BY motto
+   LIMIT 1;"
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, COUNT(species) FROM characters
+   GROUP BY species
+   ORDER BY COUNT(species) DESC
+   LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name FROM authors, subgenres
+   JOIN series
+   ON series.author_id = authors.id
+   AND series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series, characters, books
+   JOIN character_books
+   ON character_books.character_id = characters.id
+   AND character_books.book_id = books.id
+   WHERE characters.species = 'human'
+   GROUP BY species;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(books.title) FROM characters, books
+   JOIN character_books
+   ON characters.id = character_books.character_id
+   AND books.id = character_books.book_id
+   GROUP BY characters.name
+   ORDER BY COUNT(books.title) DESC, characters.name;"
 end
---
+++ lib/schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE series(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    author_id INT,
+    subgenre_id INT);
+
+CREATE TABLE subgenres(
+    id INTEGER PRIMARY KEY,
+    name TEXT);
+
+CREATE TABLE authors(
+    id INTEGER PRIMARY KEY,
+    name TEXT);
+
+CREATE TABLE books(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    year INT,
+    series_id INT);
+
+CREATE TABLE characters(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    motto TEXT,
+    species TEXT,
+    author_id INT);
+
+CREATE TABLE character_books(
+    id INTEGER PRIMARY KEY,
+    character_id INT,
+    book_id INT);
